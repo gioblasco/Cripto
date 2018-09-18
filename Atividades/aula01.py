3edb462f6d8907fac9a74ade486e1482568becfe
Giovanna Blasco <giovanna.blasco@hotmail.com>
2018-09-18 14:19:32 -0300

    editavel

--- Atividades/aula01.py
+++ Atividades/aula01.py
@@ -1,17 +1,23 @@
+# le entrada
 entrada = input()
 entrada = entrada.upper();
+
+# define sequencia de estatistica
 letras = ['A', 'E', 'O', 'S', 'R', 'D', 'N', 'I', 'T', 'M', 'U', 'L', 'C', 'V', 'P', 'G', 'Q', 'B', 'F', \
 'H', 'J', 'X', 'Z', 'K', 'Y', 'W']
 
+# contabiliza ocorrencias
 ocorrencias = [0]*26
 for i in range(0, len(entrada)):
     if entrada[i].isalpha():
         ocorrencias[ord(entrada[i])-65] += 1
 
+# printa ocorrencias
 print("\n-- LISTA DE OCORRENCIAS EM ORDEM ALFABETICA --")
 for j in range(0, 26):
     print("%c - %d" % (chr(j+65), ocorrencias[j])),
 
+# printa ocorrencias ordenadas
 ordenado = []
 print("\n-- LISTA DE OCORRENCIAS EM ORDEM DE MAIOR INCIDENCIA --")
 for k in range(0, 26):
@@ -23,9 +29,41 @@ for k in range(0, 26):
     ordenado.append(chr(ocorrencias.index(max_value)+65))
     ocorrencias[ocorrencias.index(max_value)] = -1
 
-saida = ''
-for j in range(0, len(entrada)):
-    indice = ordenado.index(entrada[j])
-    saida += letras[indice]
+# printa mensagem decifrada
+while True:
+    saida = ''
+    for j in range(0, len(entrada)):
+        indice = ordenado.index(entrada[j])
+        saida += letras[indice]
 
-print("\nMensagem decifrada: %s" % saida)
+    print("\nMensagem decifrada: %s\n" % saida)
+    
+    while True:
+        msg = input("Deseja alterar alguma letra? \n1 - Sim\n2 - Nao\n")
+        if msg != '1' and msg != '2':
+            print("Entrada invalida\n")
+            continue
+        else:
+            break
+    
+    if msg == '1':
+        while True:
+            let1 = input("Qual letra quer substituir? ")
+            if not let1.isalpha():
+                print("%s nao eh uma letra" % let1)
+                continue
+            else:
+                break
+        while True:
+            let2 = input("Por qual letra? ")
+            if not let2.isalpha():
+                print("%s nao eh uma letra" % let2)
+                continue
+            else:
+                break
+        ind1 = letras.index(let1.upper())
+        ind2 = letras.index(let2.upper())
+        letras[ind1] = let2.upper()
+        letras[ind2] = let1.upper()
+    else:
+        break
