69f54b35aeaf276cc5105b4afbed116650b5cd58
Giovanna Blasco <giovanna.blasco@hotmail.com>
2018-10-04 19:59:16 -0300

    nao acrescentei nada mt relevante

--- des/des.c
+++ des/des.c
@@ -4,23 +4,23 @@
 
 
 /* TODO:
- *  
- *  1. Fazer a permutação inicial 
+ *
+ *  1. Fazer a permutação inicial
  *      bits pares para a metade esquerda
  *      bits ímpares para a metade direita
- *  
+ *
  */
 
 // funções de tabela
 void permutacao_inicial(unsigned int *hexa);
-
+void divide_bloco(unsigned int *hexa, unsigned int *G, unsigned int *D);
 
 // funções auxiliares
 void printa_vetor(char *nome, void *vetor, int flag_unsigned);
 unsigned int reverseBits(unsigned int num);
 
 void main(){
-    unsigned int entrada[8];
+    unsigned int entrada[8], chave[8], L[4], R[4];
 
     // primeiro setando direto pra ver se os valores batem
     entrada[0] = 0x67;
@@ -37,8 +37,9 @@ void main(){
         scanf("%2x", &entrada[i]);
     }
     */
-    
+
     permutacao_inicial(entrada);
+    divide_bloco(entrada, L, R);
 
 }
 
@@ -51,14 +52,14 @@ void main(){
  *
  *  2. concatena o resultado de todas as posições com as máscaras
  *  no vetor resultante
- *  
- *  para 'concatenar' os números, devemos fazer um | entre 
- *  o número atual e o novo número (resultado da aplicação da 
+ *
+ *  para 'concatenar' os números, devemos fazer um | entre
+ *  o número atual e o novo número (resultado da aplicação da
  *  máscara)
  */
 void permutacao_inicial(unsigned int *hexa){
     /*
-        // máscaras 
+        // máscaras
         unsigned int mascara_1 = 128;
         unsigned int mascara_2 = 64;
         unsigned int mascara_3 = 32;
@@ -68,17 +69,17 @@ void permutacao_inicial(unsigned int *hexa){
         unsigned int mascara_7 = 2;
         unsigned int mascara_8 = 1;
     */
-    
+
     unsigned int mascaras[8];   // mascaras de bit
     unsigned int permutado[8];  // vetor final
     int shift_counter[8];       // vetor de valores usados nos shifts.
-    
-    // ordem das linhas da tabela inicial para a tabela final. o certo seria 2,4,6,8,1,3,5,7, 
+
+    // ordem das linhas da tabela inicial para a tabela final. o certo seria 2,4,6,8,1,3,5,7,
     // mas como o índice começa em 0, eu tirei 1 de cada posição
     static int ordem_permutacao[8] = {1,3,5,7,0,2,4,6};
 
 
-    // inicialização dos vetores 
+    // inicialização dos vetores
     // mascaras inicializadas com potências de 2
     int j = 0;
     for (int i = 7; i >= 0; i--) {
@@ -96,40 +97,49 @@ void permutacao_inicial(unsigned int *hexa){
     for (int i = 0; i < 8; i++)
         permutado[i] = 0;
 
-
-    printf("vetor permutado:\n");
+    printf("\n\nApos permutacao inicial:\n");
     for(int j = 0; j < 8; j++) {
         for(int i = 0; i < 8; i++) {
             if (shift_counter[ordem_permutacao[j]] < 0)
-                permutado[j] |= ((hexa[i]&mascaras[ordem_permutacao[j]]) << abs(shift_counter[ordem_permutacao[j]])); 
-            else  
-                permutado[j] |= ((hexa[i]&mascaras[ordem_permutacao[j]]) >> shift_counter[ordem_permutacao[j]]); 
-            
+                permutado[j] |= ((hexa[i]&mascaras[ordem_permutacao[j]]) << abs(shift_counter[ordem_permutacao[j]]));
+            else
+                permutado[j] |= ((hexa[i]&mascaras[ordem_permutacao[j]]) >> shift_counter[ordem_permutacao[j]]);
             shift_counter[ordem_permutacao[j]]++;
         }
         permutado[j] = reverseBits(permutado[j]);
-        printf("%2x ", permutado[j]);
+        printf("%4X ", permutado[j]);
     }
     printf("\n");
 }
 
-/* 
+/* passos dos rounds para criação de chave: https://br.ccm.net/contents/132-introducao-a-codificacao-des
+1. dividir o bloco de 64-bits (8 caracteres) iniciais em 2 blocos (de 32 bits) */
+void divide_bloco(unsigned int *hexa, unsigned int *G, unsigned int *D){
+  for(int i = 0; i < 4; i++)
+    G[i] = hexa[i];
+  for(int i = 4; i < 8; i++)
+    D[i] = hexa[i];
+}
+
+/* 2. função de expansão */
+
+
+/*
  * função retirada de https://www.geeksforgeeks.org/write-an-efficient-c-program-to-reverse-bits-of-a-number/
  */
-unsigned int reverseBits(unsigned int num) { 
+unsigned int reverseBits(unsigned int num) {
     // sizeof é o tamanho em BYTESSSS
-    unsigned int count = sizeof(num) * 8 - 1; 
-    unsigned int reverse_num = num; 
+    unsigned int count = sizeof(num) * 8 - 1;
+    unsigned int reverse_num = num;
 
-    num >>= 1;  
-    while(num) 
-    { 
-        reverse_num <<= 1;        
+    num >>= 1;
+    while(num)
+    {
+        reverse_num <<= 1;
         reverse_num |= num & 1;     // 1 é o bit menos significativo
-        num >>= 1; 
-        count--; 
-    } 
-    reverse_num <<= count; 
-    return reverse_num; 
-} 
-
+        num >>= 1;
+        count--;
+    }
+    reverse_num <<= count;
+    return reverse_num;
+}
