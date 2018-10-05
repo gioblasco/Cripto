7d72c85e106c67c0f179a3eca6e2ed0f816c28ad
Giovana Morais <giovana.vmorais@gmail.com>
2018-10-05 08:50:09 -0300

    correção na tipagem da entrada. unsigned int trocado pra unsigned char

--- des/des.c
+++ des/des.c
@@ -3,25 +3,17 @@
 #include <math.h>
 
 
-/* TODO:
- *
- *  1. Fazer a permutação inicial
- *      bits pares para a metade esquerda
- *      bits ímpares para a metade direita
- *
- */
-
 // funções de tabela
-void permutacao_inicial(unsigned int *hexa);
-void divide_bloco(unsigned int *hexa, unsigned int *G, unsigned int *D);
+void permutacao_inicial(unsigned char *hexa);
+//void divide_bloco(unsigned int *hexa, unsigned int *G, unsigned int *D);
 unsigned int * expansao(unsigned int *D);
 
 // funções auxiliares
-void printa_vetor(char *nome, void *vetor, int flag_unsigned);
-unsigned int reverseBits(unsigned int num);
+unsigned char reverseBits(unsigned char num);
+void print_bin(unsigned char num);
 
 void main(){
-    unsigned int entrada[8], chave[8], L[4], Lfinal[4], R[4], *E;
+    unsigned char entrada[8], chave[8], L[4], Lfinal[4], R[4], *E;
 
     // primeiro setando direto pra ver se os valores batem
     entrada[0] = 0x67;
@@ -40,36 +32,30 @@ void main(){
     */
 
     permutacao_inicial(entrada);
-    divide_bloco(entrada, L, R);
+    //divide_bloco(entrada, L, R);
     // round de 16 passos
+    /*
     for(int i = 0; i < 16; i++){
       Lfinal = R;
+      
       E = expansao(R);
-
-
-
 
       L = Lfinal;
     }
+      */
 }
 
+void permutacao_inicial(unsigned char *hexa){
 /*
- *  montar um vetor de unsigned int onde cada posição representa
- *  um número em hexa
- *
+ *  PASSOS:
  *  1. aplicar a máscara em cada posição do vetor na ordem correta
- *      (2, 4, 6, 8, 1, 3, 5  7)
- *
  *  2. concatena o resultado de todas as posições com as máscaras
  *  no vetor resultante
- *
- *  para 'concatenar' os números, devemos fazer um | entre
- *  o número atual e o novo número (resultado da aplicação da
- *  máscara)
+ *      obs: para 'concatenar' os números, devemos fazer um | entre
+ *      o número atual e o novo número (resultado da aplicação da
+ *      máscara)
  */
-void permutacao_inicial(unsigned int *hexa){
     /*
-        // máscaras
         unsigned int mascara_1 = 128;
         unsigned int mascara_2 = 64;
         unsigned int mascara_3 = 32;
@@ -80,16 +66,15 @@ void permutacao_inicial(unsigned int *hexa){
         unsigned int mascara_8 = 1;
     */
 
-    unsigned int mascaras[8];   // mascaras de bit
-    unsigned int permutado[8];  // vetor final
+    unsigned char mascaras[8];   // mascaras de bit
+    unsigned char permutado[8];  // vetor final
     int shift_counter[8];       // vetor de valores usados nos shifts.
 
-    // ordem das linhas da tabela inicial para a tabela final. o certo seria 2,4,6,8,1,3,5,7,
-    // mas como o índice começa em 0, eu tirei 1 de cada posição
+    // ordem das linhas da tabela inicial para a tabela final.
+    // 2,4,6,8,1,3,5,7
     static int ordem_permutacao[8] = {1,3,5,7,0,2,4,6};
 
 
-    // inicialização dos vetores
     // mascaras inicializadas com potências de 2
     int j = 0;
     for (int i = 7; i >= 0; i--) {
@@ -107,7 +92,7 @@ void permutacao_inicial(unsigned int *hexa){
     for (int i = 0; i < 8; i++)
         permutado[i] = 0;
 
-    printf("\n\nApos permutacao inicial:\n");
+    printf("\nApos permutacao inicial:\n");
     for(int j = 0; j < 8; j++) {
         for(int i = 0; i < 8; i++) {
             if (shift_counter[ordem_permutacao[j]] < 0)
@@ -117,14 +102,7 @@ void permutacao_inicial(unsigned int *hexa){
             shift_counter[ordem_permutacao[j]]++;
         }
         permutado[j] = reverseBits(permutado[j]);
-        printf("%4X ", permutado[j]);
-        /* se fizer:
-        hexa[j] = reverseBits(permutado[j]);
-        printf("%4X ", hexa[j]);
-
-        hexa ta ficando com valores finais errados, diferentes do permutado.
-        n entendi nada
-        */
+        printf("%2X ", permutado[j]);
     }
     printf("\n");
 }
@@ -165,7 +143,7 @@ unsigned int * xor_primeiro(unsigned int *chave, unsigned int *E){
 }
 
 /* 6. passa a chave proveniente do XOR pelas S-box */
-unsigned int * funcoes_selecao(unsigned int *){
+unsigned int * funcoes_selecao(unsigned int *chave){
 
 }
 
@@ -184,19 +162,33 @@ unsigned int * permutacao_reversa(unsigned int *G, unsigned int *D){
 /*
  * função retirada de https://www.geeksforgeeks.org/write-an-efficient-c-program-to-reverse-bits-of-a-number/
  */
-unsigned int reverseBits(unsigned int num) {
+unsigned char reverseBits(unsigned char num) {
     // sizeof é o tamanho em BYTESSSS
-    unsigned int count = sizeof(num) * 8 - 1;
-    unsigned int reverse_num = num;
+    unsigned char count = sizeof(num) * 8 - 1;
+    unsigned char reverse_num = num;
 
     num >>= 1;
-    while(num)
-    {
+    while(num) {
         reverse_num <<= 1;
         reverse_num |= num & 1;     // 1 é o bit menos significativo
         num >>= 1;
         count--;
     }
     reverse_num <<= count;
+
     return reverse_num;
+}
+
+void print_bin(unsigned char num){
+    int k;
+
+    for(int c = 31; c > 0; c--) {
+        k = num >> c;
+
+        if (k & 1)
+            printf("1");
+        else
+            printf("0");
+    }
+    printf("\n");
 }
