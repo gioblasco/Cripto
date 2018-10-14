4ab45fcbec64c953b8790f35e8933843eb45292a
Giovanna Blasco <giovanna.blasco@hotmail.com>
2018-10-14 09:30:35 -0300

    fazendo xor

--- des/des.c
+++ des/des.c
@@ -67,6 +67,8 @@ void main(){
     }
     */
 
+    xor_primeiro(entrada, chave, Lfinal);
+
     printf("CHAVE\n");
     print_saida(chave, 8);
 
@@ -79,7 +81,7 @@ void main(){
         atribui(Lfinal, R, 4);
         expansao(R, E);
         // printf("Vetor expandido: \n");
-        // print_saida(E, 6);    
+        // print_saida(E, 6);
         atribui(L, Lfinal, 4);
     }
 }
@@ -174,23 +176,23 @@ void expansao(unsigned char *D, unsigned char *E){
     }
 }
 
-/* 3. permutação inicial de chave (PC-1) 
+/* 3. permutação inicial de chave (PC-1)
  *  seleciona 56 bits da chave de 64 e divide em 2 blocos
  *  de 28 bits.
  */
 void pc1(unsigned char *chave, unsigned char *pc_1){
     unsigned char PC_1[56] = {
-                                57, 49, 41, 33, 25, 17, 9, 
-                                1, 58, 50, 42, 34, 26, 18, 
-                                10, 2, 59, 51, 43, 35, 27, 
+                                57, 49, 41, 33, 25, 17, 9,
+                                1, 58, 50, 42, 34, 26, 18,
+                                10, 2, 59, 51, 43, 35, 27,
                                 19, 11, 3, 60, 52, 44, 36,
-                                63, 55, 47, 39, 31, 23, 15, 
+                                63, 55, 47, 39, 31, 23, 15,
                                 7, 62, 54, 46, 38, 30, 22,
-                                14, 6, 61, 53, 45, 37, 29, 
-                                21, 13, 5, 28, 20, 12, 4 
+                                14, 6, 61, 53, 45, 37, 29,
+                                21, 13, 5, 28, 20, 12, 4
                             };
 
-    
+
     unsigned char temp_bit = 0;
     unsigned char permutado[7] = {0, 0, 0, 0, 0, 0, 0};
     unsigned int mascara[9] = {1, 256, 128, 64, 32, 16, 8, 4, 2};
@@ -214,20 +216,25 @@ void pc1(unsigned char *chave, unsigned char *pc_1){
 
 void pc2(unsigned char *chave){
     unsigned char PC_2[48] = {
-                                14, 17, 11, 24, 1, 5, 
-                                3, 28, 15, 6, 21, 10, 
-                                23, 19, 12, 4, 26, 8, 
-                                16, 7, 27, 20, 13, 2, 
-                                41, 52, 31, 37, 47, 55, 
-                                30, 40, 51, 45, 33, 48, 
-                                44, 49, 39, 56, 34, 53, 
+                                14, 17, 11, 24, 1, 5,
+                                3, 28, 15, 6, 21, 10,
+                                23, 19, 12, 4, 26, 8,
+                                16, 7, 27, 20, 13, 2,
+                                41, 52, 31, 37, 47, 55,
+                                30, 40, 51, 45, 33, 48,
+                                44, 49, 39, 56, 34, 53,
                                 46, 42, 50, 36, 29, 32
                             };
 
 }
 
 /* 5. XOR entre texto expandido e chave (ambos 48 bits) */
-void * xor_primeiro(unsigned char *chave, unsigned char *E){
+void * xor_primeiro(unsigned char *chave, unsigned char *E, unsigned char *after_xor1){
+  unsigned char c[8] = { 0x50, 0x2C, 0xAC, 0x57, 0x2A, 0xC2 };
+  unsigned char ex[8] = { 0x00, 0x17, 0xFE, 0x8A, 0x7C, 0xF4 };
+  for(int i = 0; i < 6; i++){
+    after_xor1[i] = c[i] ^ ex[i];
+  }
 }
 
 /* 6. passa a resultado proveniente do XOR pelas S-box */
